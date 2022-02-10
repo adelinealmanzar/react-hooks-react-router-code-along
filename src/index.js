f60642c163873aabf7c66264caf438cca8ffb1a2
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-10 12:36:49 -0800

    commit tree 73ebceec359c1b6d266822d860acc95bd44b657f

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+import App from "./App";
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
